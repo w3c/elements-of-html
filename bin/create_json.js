7f5d7a9a85dfbe37c2034123942cb7ea47817b8f
Jon Schlinkert <dev@sellside.com>
2017-10-31 08:27:36 -0400

    update build script  (#20)
    * update script to use cheerio
    
    fwiw, this gets around the need for jsdom which requires node-gyp. Many users are unable to install node-gyp, especially windows users, although today I also had problems installing node-gyp on my macbook pro running the latest version of macOS High Sierra.
    
    * run build script
    
    * add instructions to readme

--- bin/create_json.js
+++ bin/create_json.js
@@ -1,76 +1,64 @@
 #!/usr/bin/env node
 
-var fs = require("fs");
-var jsdom = require("jsdom");
-var src = fs.readFileSync(__dirname+"/../index.html", "utf-8");
+var fs = require('fs');
+var path = require('path');
+var cheerio = require('cheerio');
 
-/* read the index file and create a DOM for it, process the data */
-jsdom.env(src, function(err, window) {
-  if (err) {
-    throw err;
-  }
+var destPath = path.join(__dirname, '../elements.json');
+var srcPath = path.join(__dirname, '../index.html');
 
-  /* poor man's jQuery */
-  function $(selector, ctx) {
-    ctx = ctx || window.document;
-    return ctx.querySelectorAll(selector);
-  }
+var src = fs.readFileSync(srcPath, 'utf-8');
+var $ = cheerio.load(src);
 
-  var trs = $('main tbody tr'),
-      i = 0,
-      j = trs.length,
-      data = [],
-      /* the list of specification names from thead (first th is "Element") */
-      speclist = Array.prototype.map.call(
-                   $('main thead th:not(:first-child)'),
-                   function(th) {
-                     return th.textContent.replace(/[^0-9.]+/, function(m) {
-                       if (m.substr(0, 1) === 'X') {
-                         return 'X';
-                       } else {
-                         return '';
-                       }
-                     });
-                   }),
-      dataset, tds, k, l;
-
-  for (; i < j; i++) {
-    dataset = {
-      element: $('th', trs[i])[0]
-                 .textContent
-                 .replace(/ \[(.*)\]/, function(m, m1) {
-                   /* for qualified <input> elements, change to valid
-                    * CSS selector */
-                   return '[type="'+m1+'"]';
-                 })
-    };
-    if ($('th a', trs[i]).length) {
-      /* extract the defining doc's URL.
-       * TODO some elements have more than one URL */
-      dataset.link = $('th a', trs[i])[0].href;
+var data = [];
+var trs = [].slice.call($('main tbody tr'));
+var speclist = [].map.call($('main thead th:not(:first-child)'), function(th) {
+  return $(th).text().replace(/[^0-9.]+/, function(m) {
+    if (m.substr(0, 1) === 'X') {
+      return 'X';
+    } else {
+      return '';
     }
+  });
+});
 
-    dataset.specs = [];
-    tds = $('td', trs[i]);
-    k = tds.length;
+for (var i = 0; i < trs.length; i++) {
+  var tr = trs[i];
+  var ele = $('th', tr)[0];
+  var element = $(ele).text();
 
-    for (l = 0; l < k; l++) {
-      if ($('img', tds[l]).length &&
-          $('img', tds[l])[0].alt.substr(0, 3) === 'yes') {
+  var dataset = {
+    element: element.replace(/ \[(.*)\]/, function(m, $1) {
+      /* for qualified <input> elements, change to valid CSS selector */
+      return '[type="' + $1 + '"]';
+    }),
+    link: '',
+    specs: []
+  };
+
+  /* extract the defining doc's URL. TODO some elements have more than one URL */
+  $('th a', tr).each(function(idx, ele) {
+    var attr = $(ele).attr();
+    if (!dataset.link && attr && attr.href) {
+      dataset.link = attr.href;
+    }
+  });
+
+  var tds = [].slice.call($('td', tr));
+
+  for (var n = 0; n < tds.length; n++) {
+    var td = tds[n];
+
+    $('img', td).each(function(idx, ele) {
+      var attr = $(ele).attr();
+      if (attr && attr.alt && attr.alt.slice(0, 3) === 'yes') {
         /* if it's a "yes" image, add the spec to the list for this element */
-        dataset.specs.push(speclist[l]);
-      }
-    }
-
-    data.push(dataset);
-  }
-
-  fs.writeFile(
-    __dirname+'/../elements.json',
-    JSON.stringify(data, null, '  '),
-    function (err) {
-      if (err) {
-        throw err;
+        dataset.specs.push(speclist[n]);
       }
     });
-});
+  }
+
+  data.push(dataset);
+}
+
+fs.writeFileSync(destPath, JSON.stringify(data, null, '  '));
